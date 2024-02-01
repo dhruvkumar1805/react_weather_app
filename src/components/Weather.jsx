bee5b32b8d1c811cdff9e00c4c2cab73671335f0
dhruvkumar1805 <dhruvkumar1805@gmail.com>
2024-02-01 23:29:15 +0530

    Inital push

---
+++ src/components/Weather.jsx
@@ -0,0 +1,201 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  IoMdSunny,
+  IoMdRainy,
+  IoMdCloudy,
+  IoMdThunderstorm,
+  IoMdSearch,
+} from "react-icons/io";
+import {
+  BsCloudHaze2Fill,
+  BsCloudDrizzleFill,
+  BsEye,
+  BsWater,
+  BsThermometer,
+  BsWind,
+  BsSnow,
+} from "react-icons/bs";
+import { TbTemperatureCelsius } from "react-icons/tb";
+import { ImSpinner8 } from "react-icons/im";
+
+const APIkey = process.env.VITE_BACKEND_URL;
+
+const Weather = () => {
+  const [data, setData] = useState(null);
+  const [location, setLocation] = useState("Punjab");
+  const [inputValue, setInputValue] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    if (inputValue !== "") {
+      setLocation(inputValue);
+    }
+
+    const input = document.querySelector("input");
+    input.value = "";
+
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    setLoading(true);
+
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIkey}`;
+
+    axios.get(url).then((res) => {
+      setData(res.data);
+      setLoading(false);
+    });
+  }, [location]);
+
+  if (!data) {
+    return (
+      <div className="w-full h-screen bg-gradient-to-b from-blue-500 to-blue-700 flex flex-col justify-center items-center">
+        <div>
+          <ImSpinner8 className="text-5xl animate-spin text-white" />
+        </div>
+      </div>
+    );
+  }
+
+  let icon;
+  switch (data.weather[0].main) {
+    case "Clouds":
+      icon = <IoMdCloudy />;
+      break;
+
+    case "Haze":
+      icon = <BsCloudHaze2Fill />;
+      break;
+
+    case "Rain":
+      icon = <IoMdRainy />;
+      break;
+
+    case "Clear":
+      icon = <IoMdSunny />;
+      break;
+
+    case "Drizzle":
+      icon = <BsCloudDrizzleFill />;
+      break;
+
+    case "Snow":
+      icon = <BsSnow />;
+      break;
+
+    case "ThunderStorm":
+      icon = <IoMdThunderstorm />;
+      break;
+  }
+
+  const date = new Date();
+
+  return (
+    <div className="w-full h-screen bg-gradient-to-b from-blue-500 to-blue-700 flex flex-col items-center justify-center px-4 lg:px-0">
+      <form className="h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-6">
+        <div className="h-full relative flex items-center justify-between p-2">
+          <input
+            onChange={(e) => handleInput(e)}
+            className="flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full"
+            type="text"
+            placeholder="Search by city or country"
+          />
+          <button
+            onClick={(e) => handleSubmit(e)}
+            className="bg-[#a1e7ff] hover:bg-[#cacaca] transition-all duration-500 ease-in-out w-20 h-12 rounded-full flex justify-center items-center"
+          >
+            <IoMdSearch className="text-2xl" />
+          </button>
+        </div>
+      </form>
+
+      <div className="w-full max-w-[450px] bg-black/20 min-h-[550px] text-white backdrop-blur-[32px] rounded-[32px] py-6 px-6">
+        {loading ? (
+          <div className="w-full h-full flex justify-center items-center">
+            <ImSpinner8 className="text-white text-5xl animate-spin" />
+          </div>
+        ) : (
+          <div className="font-Poppins">
+            <div className="flex items-center gap-x-5">
+              <div className="text-[87px]">{icon}</div>
+              <div>
+                <div className="text-2xl font-semibold">
+                  {data.name}, {data.sys.country}
+                </div>
+                <div>
+                  {date.getUTCDate()}/{date.getUTCMonth() + 1}/
+                  {date.getUTCFullYear()}
+                </div>
+              </div>
+            </div>
+            <div className="my-20">
+              <div className="flex justify-center items-center">
+                <div className="text-[144px] leading-none font-light">
+                  {parseInt(data.main.temp)}
+                </div>
+                <div className="text-4xl">
+                  <TbTemperatureCelsius />
+                </div>
+              </div>
+              <div className="capitalize text-center">
+                {data.weather[0].description}
+              </div>
+            </div>
+            <div className="max-w-[378px] mx-auto flex flex-col gap-y-6">
+              <div className="flex justify-between">
+                <div className="flex items-center gap-x-2">
+                  <div className="text-[20px]">
+                    <BsEye />
+                  </div>
+                  <div>
+                    Visibility
+                    <span className="ml-2">{data.visibility / 1000} Km</span>
+                  </div>
+                </div>
+                <div className="flex items-center gap-x-2">
+                  <div className="text-[20px]">
+                    <BsThermometer />
+                  </div>
+                  <div className="flex ml-2">
+                    Feels like
+                    <div className="flex ml-2">
+                      {parseInt(data.main.feels_like)}
+                      <TbTemperatureCelsius />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex justify-between">
+                <div className="flex items-center gap-x-2">
+                  <div className="text-[20px]">
+                    <BsWater />
+                  </div>
+                  <div>
+                    Humidity<span className="ml-2">{data.main.humidity} %</span>
+                  </div>
+                </div>
+                <div className="flex items-center gap-x-2">
+                  <div className="text-[20px]">
+                    <BsWind />
+                  </div>
+                  <div>
+                    Wind<span className="ml-2">{data.wind.speed} m/s</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
